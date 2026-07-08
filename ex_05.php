ff957e9a2f74c5c276c83e1d7f1c6924b58e1bb8
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-07-08 08:54:52 -0300

    up - arrumei a 5

--- ex_05.php
+++ ex_05.php
@@ -7,11 +7,11 @@ function  analisarTexto($texto)
     $vogais = preg_match_all('/[aeiou]/i', $texto);
     $consoantes = preg_match_all('/[bcdfghjklmnpqrstvwxyz]/i', $texto);
 
-    return "O texto tem $caracteres caracteres, $palavras palavras, $frases frases, $vogais vogais e $consoantes consoantes.";
+    return "caracteres: $caracteres,palavras: $palavras ,frases: $frases ,vogais: $vogais,consoantes: $consoantes";
 
 }
 
-$texto = "Toca no gk fei.";
+$texto = "mais de uma hora só para entender esses bgl";
 
 echo "Texto original: $texto <br>";
 echo analisarTexto($texto);

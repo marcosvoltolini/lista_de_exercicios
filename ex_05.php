f7a78d7f25777d656e879218b9518ea282050ef8
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-07-08 08:47:59 -0300

    add - terminei a 5

--- ex_05.php
+++ ex_05.php
@@ -1,7 +1,15 @@
 <?php
-function analisarTexto(){
-    $texto = "A sexta estrela não esta na camisa, porque está no campo (Neymar)";
+function  analisarTexto(){
+    $texto = "Mas o filha da puta era de raça (de raça) Me envolvi com o Conde Drácula (Drácula) Fiquei com o vampiro de Madureira Chupou minha buceta menstruada Filha da puta era de raça (de raça) Me envolvi com o Conde Drácula (Drácula) Fiquei com o vampiro de Madureira Chupou minha buceta menstruada.";
     $tamanho = strlen($texto);
-    $palavra = str_word_count($texto);
-    
+    $palavras = str_word_count($texto);
+    $vogais = preg_match_all('/[aeiouAEIOU]/', $texto);
+    $consoantes = preg_match_all('/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/', $texto);
+
+    echo "Texto: " . $texto . "<br>";
+    echo "Tamanho: " . $tamanho . "<br>";
+    echo "Número de palavras: " . $palavras . "<br>";
+    echo "Número de vogais: " . $vogais . "<br>";
+    echo "Número de consoantes: " . $consoantes . "<br>";
 }
+echo analisarTexto();

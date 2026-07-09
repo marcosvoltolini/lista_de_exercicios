fe2817310db9ac0cbdd10dcbf16fd369665be885
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-07-09 07:57:01 -0300

    add - voltei com a 9

--- ex_09.php
+++ ex_09.php
@@ -1,2 +1,31 @@
+<?php
 
+function analisarNumero($numero) {
+ 
+$parOuImpar = ($numero % 2 == 0) ? "par" : "ímpar";
+
+$primo= true;
+if ($numero <= 1) {
+    $primo = false;
+} else {
+    for ($i = 2; $i <= sqrt($numero); $i++) {
+        if ($numero % $i == 0) {
+            $primo = false;
+            break;
+        }
+    }
+}
+    $numeroPrimo = $primo ? "é primo" : "não é primo";
+
+$somaDivisores = 0;
+for ($i = 1; $i < $numero; $i++) {
+    if ($numero % $i == 0) {
+        $somaDivisores += $i;
+    }
+}
+$numeroPerfeito = ($somaDivisores == $numero) ? "é perfeito" : "não é perfeito";
+
+
+ return [$parOuImpar, $numeroPrimo, $numeroPerfeito];
+}
 
